b3e537ef06b8e5ea3c6ea6275d77ad0afaa4206c
Matthew Pickering <matthewtpickering@gmail.com>
2018-07-01 16:03:59 +0000

    Use a submodule instead of fetchgit

--- haskell-plugins/default.nix
+++ haskell-plugins/default.nix
@@ -1,3 +1,1 @@
-let
-  plugin-overlay-git = builtins.fetchGit { url = https://github.com/mpickering/haskell-nix-plugin.git;};
-in import "${plugin-overlay-git}/overlay.nix"
+import "./haskell-nix-plugin/overlay.nix"
