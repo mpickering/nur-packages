afca2322292de36c0a5150f2e498cdfae9b9986f
Matthew Pickering <matthewtpickering@gmail.com>
2019-01-16 13:39:16 +0000

    Add ghc-artefact-nix

--- default.nix
+++ default.nix
@@ -1,4 +1,8 @@
 { pkgs }:
 {
-  overlays = { haskell-plugins = import ./haskell-plugins ;};
+  overlays = { haskell-plugins = import ./haskell-plugins; };
+
+  ghcHEAD = pkgs.callPackage ./ghc-artefact-nix/artifact.nix {} {};
+
+  ghcHEADAt = pkgs.callPackage ./ghc-artefact-nix/artifact.nix {};
 }
