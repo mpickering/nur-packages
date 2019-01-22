42ca9ae1cc3851e1e51d96bab9f57be4e14e9d2f
Matthew Pickering <matthewtpickering@gmail.com>
2019-01-22 16:28:05 +0000

    Add ghc-head-from

--- default.nix
+++ default.nix
@@ -2,7 +2,5 @@
 {
   overlays = { haskell-plugins = import ./haskell-plugins; };
 
-  ghcHEAD = pkgs.callPackage ./ghc-artefact-nix/artifact.nix {} {};
-
-  ghcHEADAt = pkgs.callPackage ./ghc-artefact-nix/artifact.nix {};
+  ghc-head-from = pkgs.callPackage ./ghc-artefact-nix/ghc-head-from.nix {};
 }
